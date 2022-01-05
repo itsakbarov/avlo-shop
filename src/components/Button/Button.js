0b297a0e87c64c5abc2557bc555a2fc8743783bc
theAkbarov <asrorjon5538@gmail.com>
2022-01-05 17:43:45 +0500

    base setup

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import {StyledButton} from "./style";
+
+const Button = ({children}) => {
+    return (
+        <StyledButton>
+            {children}
+        </StyledButton>
+    )
+}
+export default Button
---
+++ src/components/Button/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  background: #FF3D00;
+  border-radius: 5px;
+  font-family: Gilroy;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  padding: 12px 27px;
+  line-height: 20px;
+  border: none;
+  color: #FFFFFF;
+  position: relative;
+  &:before {
+    content: "";
+    left: 5px;
+    bottom: -6px;
+    position: absolute;
+    background: transparent;
+    border: 3px solid #FF3D00;
+    box-sizing: border-box;
+    border-radius: 5px;
+    width: 100%;
+    height: 100%;
+  }
+`
